151d68eb90b2197695ac110f2f0f983a4888167e
alexxirou <alex.xirou@hotmail.com>
2024-01-25 18:50:21 +0200

    chaged api endpoint to support sessions

--- Control_module_of_API_endpoint.rb
+++ Control_module_of_API_endpoint.rb
@@ -4,17 +4,21 @@ require_relative 'Model.rb'
 require_relative 'Random_ship_generator.rb'
 require_relative 'Opponent_logic.rb'
 
-set :port, 4567 
+enable :sessions
+set :port, 4567
 ai = AI.new
 
 post '/api/move' do
-  player_result = initial_game_data[:ai_grid].shoot_at_position([params[:shoot_position].split(',').map(&:to_i)])
-  opponent_sunken_ships = ai.sunken_ships_positions.to_a if player_result[0] == 'DESTROYED'
-  ai_shoot_position = ai.next_shot(initial_game_data[:player_grid])
-  ai_result = initial_game_data[:player_grid].shoot_at_position(ai_shoot_position)
-  ai.record_shot(ai_shoot_position, ai_result, initial_game_data[:player_grid])
-  player_sunken_ships = initial_game_data[:player_grid].sunken_ships_positions.to_a if ai_result[0] == 'DESTROYED'
-  
+  session[:game_state] ||= initialize_game_state(params['gridSize'].to_i, params['NumShips'].to_i)
+  game_state = session[:game_state]
+
+  player_result = game_state[:ai_grid].shoot_at_position([params['shoot_position'].split(',').map(&:to_i)])
+  opponent_sunken_ships = game_state[:ai_grid].sunken_ships_positions.to_a if player_result[0] == 'DESTROYED'
+  ai_shoot_position = ai.next_shot(game_state[:player_grid])
+  ai_result = game_state[:player_grid].shoot_at_position(ai_shoot_position)
+  ai.record_shot(ai_shoot_position, ai_result, game_state[:player_grid])
+  player_sunken_ships = game_state[:player_grid].sunken_ships_positions.to_a if ai_result[0] == 'DESTROYED'
+
   content_type :json
   {
     player_result: player_result,
@@ -26,25 +30,26 @@ post '/api/move' do
 end
 
 get '/initial_game_data' do
-  content_type :json
-  
-  # Create grids
-  player_grid = Grid.new(params['gridSize'].to_i, params['gridSize'].to_i)
-  ai_grid = Grid.new(params['gridSize'].to_i, params['gridSize'].to_i)
+  session[:game_state] ||= initialize_game_state(params['gridSize'].to_i, params['NumShips'].to_i)
+  game_state = session[:game_state]
 
-  # Generate ship arrays based on the number of ships data from the frontend
-  player_ships_array = RandomShipGenerator.new.generate_random_ships(params['NumShips'])
-  ai_ships_array = RandomShipGenerator.new.generate_random_ships(params['NumShips'])
+  content_type :json
+  {
+    player_grid: game_state[:player_grid],
+    ai_grid: game_state[:ai_grid]
+    # Exclude player_ships_array from the response
+  }.to_json
+end
+
+def initialize_game_state(grid_size, num_ships)
+  player_grid = Grid.new(grid_size, grid_size)
+  ai_grid = Grid.new(grid_size, grid_size)
+
+  player_ships_array = RandomShipGenerator.new.generate_random_ships(num_ships)
+  ai_ships_array = RandomShipGenerator.new.generate_random_ships(num_ships)
 
   player_ships_array.each { |ship| player_grid.add_ship_to_grid(ship) }
   ai_ships_array.each { |ship| ai_grid.add_ship_to_grid(ship) }
 
-  # You can include other necessary information in the initial game data
-  initial_game_data = {
-    player_grid: player_grid,
-    ai_grid: ai_grid
-    # Exclude player_ships_array from the response
-  }
-
-  initial_game_data.to_json
+  { player_grid: player_grid, ai_grid: ai_grid }
 end
