4af02d4b3216f79b041be4c6cb69393893c1a31b
alexxirou <alex.xirou@hotmail.com>
2024-02-04 00:42:41 +0200

    Readded comments in Opponent_logic.rb

--- Opponent_logic.rb
+++ Opponent_logic.rb
@@ -1,17 +1,29 @@
 require 'set'
 
 class AI
+  #
+  # Initializes a new instance of the AI class.
+  #
   def initialize
     @grid_hits = Set.new
     @last_hit_position = nil
     @ship_orientation = nil
   end
 
+  # Gets the next shot for the AI based on the current state of the grid.
+  #
+  # @param [Grid] grid The current state of the grid.
+  # @return [Array<Integer, Integer>] The target position for the next shot.
   def next_shot(grid)
     target_position = highest_probability_position(grid)
     target_position
   end
 
+  # Records the result of a shot fired by the AI.
+  #
+  # @param [Array<Integer, Integer>] shot The coordinates of the shot.
+  # @param [String] result The result of the shot ('HIT', 'MISS', 'DESTROYED').
+  # @param [Grid] grid The current state of the grid.
   def record_shot(shot, result, grid)
     if result == 'HIT' || result == 'DESTROYED'
       update_ship_orientation(shot, grid)
@@ -22,6 +34,10 @@ class AI
 
   private
 
+  # Determines the highest probability position for the next shot.
+  #
+  # @param [Grid] grid The current state of the grid.
+  # @return [Array<Integer, Integer>] The target position for the next shot.
   def highest_probability_position(grid)
     hits = @grid_hits
     adjacent_shot = target_around_last_hit(grid)
@@ -29,6 +45,10 @@ class AI
     return adjacent_shot || random_position(grid)
   end
 
+  # Generates a random position on the grid.
+  #
+  # @param [Grid] grid The current state of the grid.
+  # @return [Array<Integer, Integer>] The randomly chosen position.
   def random_position(grid)
     all_possible_positions = all_possible_positions(grid)
     mid_x = grid.size_x / 2
@@ -50,10 +70,18 @@ class AI
     all_possible_positions.reject { |pos| grid.misses.include?(pos) || @grid_hits.include?(pos) }.sample
   end
 
+  # Gets all possible positions on the grid.
+  #
+  # @param [Grid] grid The current state of the grid.
+  # @return [Array<Array<Integer, Integer>>] All possible positions on the grid.
   def all_possible_positions(grid)
     (0..grid.size_x - 1).to_a.product((0..grid.size_y - 1).to_a)
   end
 
+  # Determines the target positions around the last hit.
+  #
+  # @param [Grid] grid The current state of the grid.
+  # @return [Array<Integer, Integer>] The target position around the last hit.
   def target_around_last_hit(grid)
     return nil if @last_hit_position.nil? || grid_observers_include_last_hit?(grid)
 
@@ -83,6 +111,10 @@ class AI
     return potential_positions.sample if potential_positions&.any?
   end
 
+  # Updates the ship orientation based on the last hit and current grid state.
+  #
+  # @param [Array<Integer, Integer>] last_hit The coordinates of the last hit.
+  # @param [Grid] grid The current state of the grid.
   def update_ship_orientation(last_hit, grid)
     @ship_orientation = nil
     return if @last_hit_position.nil? || grid_observers_include_last_hit?(grid)
@@ -97,6 +129,10 @@ class AI
     end
   end
 
+  # Checks if any grid observers include the last hit position.
+  #
+  # @param [Grid] grid The current state of the grid.
+  # @return [Boolean] True if any observer includes the last hit position, false otherwise.
   def grid_observers_include_last_hit?(grid)
     grid.observers.any? { |observer| observer.positions.include?(@last_hit_position) }
   end
